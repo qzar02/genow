104ad48940c50906d92dd693f2d87cbd00df073b
Leandro Soares <qzar02@gmail.com>
2020-02-20 20:11:50 -0300

    criando domain da api

--- model/api.py
+++ model/api.py
@@ -1,8 +1,14 @@
-from eve import Eve
 import yaml
+from eve import Eve
 
-with open('domain.yaml') as domain:
-    settings = yaml.load(domain)
+with open('domain.yaml') as fdomain:
+    domain = yaml.load(fdomain)
 
-app = Eve(settings=settings)
+app = Eve(settings={
+    'MONGO_HOST': 'localhost',
+    'MONGO_PORT': 27017,
+    'MONGO_DBNAME': 'genow',
+    'DOMAIN': domain
+})
+
 app.run()
