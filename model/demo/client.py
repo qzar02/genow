8003faf04b0462432edf388771c6f50a822b17be
Leandro Soares <qzar02@gmail.com>
2020-02-28 02:02:03 -0300

    adicionando renders e templates para dbdiagram.io

---
+++ model/demo/client.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import yaml
+
+URL_API = "http://localhost:5000"
+
+session = requests.Session()
+with open('dw.yaml') as demo_dw:
+    dw_model = yaml.load(demo_dw.read())
+
+for table in dw_model.get('tables'):
+    r = session.post(
+        f"{URL_API}/table",
+        data=json.dumps(table),
+        headers={'content-type': 'application/json'})
+    print(r.json())
+
+for relation in dw_model.get('relationships'):
+    r = session.post(
+        f"{URL_API}/relationship",
+        data=json.dumps(relation),
+        headers={'content-type': 'application/json'})
+    print(r.json())
---
+++ model/demo/renders/dbdiagram.py
@@ -0,0 +1,22 @@
+import jinja2
+import requests
+
+URL_API = "http://localhost:5000"
+
+session = requests.Session()
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('../../templates/'))
+
+tables = session.get(f"{URL_API}/table").json()['_items']
+relationships = session.get(f"{URL_API}/relationship").json()['_items']
+
+render = (
+    env
+    .get_template('dbdiagram/DER.jinja2')
+    .render(
+        tables=tables,
+        relationships=relationships)
+)
+
+with open('dbdiagram.der', 'w') as f:
+    f.write(render)
